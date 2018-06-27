9379f5f0353f6c95f3fc30ba15b22c9b97d786cc
Keano <keanov@gmail.com>
2018-06-27 10:49:17 -0700

    init repo

---
+++ teams.js
@@ -0,0 +1,130 @@
+var teams = [
+    {
+      "name": "Egypt",
+      "color" : '#CD2A37'
+    },
+    {
+      "name": "Iceland",
+      "color" : '#052AF9'
+    },
+    {
+      "name": "Tunisia",
+      "color" : '#FF0010'
+    },
+    {
+      "name": "Uruguay",
+      "color" : '#7CB5FC'
+    },
+    {
+      "name": "Nigeria",
+      "color" : '#184031'
+    },
+    {
+      "name": "Russia",
+      "color" : '#FF0010'
+    },
+    {
+      "name": "Peru",
+      "color" : '#FFFFFF'
+    },
+    {
+      "name": "Panama",
+      "color" : '#FF0010'
+    },
+    {
+      "name": "Sweden",
+      "color" : '#FFEC3D'
+    },
+    {
+      "name": "Saudi Arabia",
+      "color" : '#008E46'
+    },
+    {
+      "name": "Croatia",
+      "color" : '#1A2B70'
+    },
+    {
+      "name": "Spain",
+      "color" : '#FF0010'
+    },
+    {
+      "name": "Morocco",
+      "color" : '#D8001A'
+    },
+    {
+      "name": "Argentina",
+      "color" : '#9DCFFD'
+    },
+    {
+      "name": "South Korea",
+      "color" : '#E60028'
+    },
+    {
+      "name": "Switzerland",
+      "color" : '#FF2433'
+    },
+    {
+      "name": "Senegal",
+      "color" : '#00905D'
+    },
+    {
+      "name": "Australia",
+      "color" : '#FFBC30'
+    },
+    {
+      "name": "Colombia",
+      "color" : '#FFFF42'
+    },
+    {
+      "name": "Belgium",
+      "color" : '#FF0010'
+    },
+    {
+      "name": "Costa Rica",
+      "color" : '#E5000D'
+    },
+    {
+      "name": "Japan",
+      "color" : '#00007D'
+    },
+    {
+      "name": "Brazil",
+      "color" : '#FFE43A'
+    },
+    {
+      "name": "Poland",
+      "color" : '#FCFCFC'
+    },
+    {
+      "name": "Portugal",
+      "color" : '#EF002B'
+    },
+    {
+      "name": "Iran",
+      "color" : '#FFFFFF'
+    },
+    {
+      "name": "England",
+      "color" : '#D3000B'
+    },
+    {
+      "name": "Germany",
+      "color" : '#00889A'
+    },
+    {
+      "name": "Mexico",
+      "color" : '#164D3F'
+    },
+    {
+      "name": "Denmark",
+      "color" : '#FF0010'
+    },
+    {
+      "name": "France",
+      "color" : '#0F2853'
+    },
+    {
+      "name": "Serbia",
+      "color" : '#FF0013'
+    }
+  ]
